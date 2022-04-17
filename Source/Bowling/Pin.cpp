466c9ce6773be2c3d1889633776411dad7ac379c
Jaiden Gerig <jaideng123@yahoo.com>
2022-04-16 17:02:23 -0700

    Tweaked ball size + pin center of gravity

---
+++ Source/Bowling/Pin.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pin.h"
+
+
+// Sets default values
+APin::APin()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void APin::BeginPlay()
+{
+	Super::BeginPlay();
+	PrimitiveComponent = Cast<UPrimitiveComponent>(GetComponentByClass(UPrimitiveComponent::StaticClass()));
+}
+
+// Called every frame
+void APin::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	DrawDebugCircle(GetWorld(), PrimitiveComponent->GetCenterOfMass(), 4.0, 10, FColor::Magenta,false,-1,1,1);
+}
---
+++ Source/Bowling/Pin.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pin.generated.h"
+
+UCLASS()
+class BOWLING_API APin : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleInstanceOnly)
+	UPrimitiveComponent* PrimitiveComponent;
+	
+	// Sets default values for this actor's properties
+	APin();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
