e23cf9de780815d0efdebf73da72f8127adfcf62
Jaiden Gerig <jaideng123@yahoo.com>
2024-08-11 21:18:43 -0700

    Update DUETween.cpp

--- Source/DUETween/Private/DUETween.cpp
+++ Source/DUETween/Private/DUETween.cpp
@@ -4,25 +4,32 @@
 
 void FDUETweenModule::StartupModule()
 {
-    UE_LOG(LogTemp, Display, TEXT("Loaded DUETween"));
+	UE_LOG(LogTemp, Display, TEXT("Loaded DUETween"));
 
-    TickDelegate = FTickerDelegate::CreateRaw( this, &FDUETweenModule::Tick );
-    TickDelegateHandle = FTSTicker::GetCoreTicker().AddTicker( TickDelegate );
+	TickDelegate = FTickerDelegate::CreateRaw(this, &FDUETweenModule::Tick);
+	TickDelegateHandle = FTSTicker::GetCoreTicker().AddTicker(TickDelegate);
 }
 
 void FDUETweenModule::ShutdownModule()
 {
-    UE_LOG(LogTemp, Display, TEXT("UnLoaded DUETween"));
-    
-    FTSTicker::GetCoreTicker().RemoveTicker(TickDelegateHandle);
+	UE_LOG(LogTemp, Display, TEXT("UnLoaded DUETween"));
+
+	FTSTicker::GetCoreTicker().RemoveTicker(TickDelegateHandle);
 }
 
 bool FDUETweenModule::Tick(float deltaTime)
 {
-    UE_LOG(LogTemp, Display, TEXT("Ticking DUETween"));
-    return true;
+	// Only Run Tweans During Play
+	if (GEngine->GetCurrentPlayWorld())
+	{
+		return true;
+	}
+
+	UE_LOG(LogTemp, Display, TEXT("Ticking DUETween"));
+
+	return true;
 }
 
 #undef LOCTEXT_NAMESPACE
-    
+
 IMPLEMENT_MODULE(FDUETweenModule, DUETween)
