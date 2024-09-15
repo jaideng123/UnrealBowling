4263a186ee21fca9f7058294a6c248059e4071b8
Jaiden Gerig <jaideng123@yahoo.com>
2024-09-14 19:39:37 -0700

    Update DUEEasingFunctionLibrary.h

--- Source/DUETween/Public/DUEEasingFunctionLibrary.h
+++ Source/DUETween/Public/DUEEasingFunctionLibrary.h
@@ -1,6 +1,57 @@
 #pragma once
 #include "Kismet/KismetMathLibrary.h"
 
+
+
+// DoTweenEasings for Ref:
+// public enum Ease
+//   {
+//     Unset,
+//     Linear,
+//     InSine,
+//     OutSine,
+//     InOutSine,
+//     InQuad,
+//     OutQuad,
+//     InOutQuad,
+//     InCubic,
+//     OutCubic,
+//     InOutCubic,
+//     InQuart,
+//     OutQuart,
+//     InOutQuart,
+//     InQuint,
+//     OutQuint,
+//     InOutQuint,
+//     InExpo,
+//     OutExpo,
+//     InOutExpo,
+//     InCirc,
+//     OutCirc,
+//     InOutCirc,
+//     InElastic,
+//     OutElastic,
+//     InOutElastic,
+//     InBack,
+//     OutBack,
+//     InOutBack,
+//     InBounce,
+//     OutBounce,
+//     InOutBounce,
+//     Flash,
+//     InFlash,
+//     OutFlash,
+//     InOutFlash,
+//     /// <summary>
+//     /// Don't assign this! It's assigned automatically when creating 0 duration tweens
+//     /// </summary>
+//     INTERNAL_Zero,
+//     /// <summary>
+//     /// Don't assign this! It's assigned automatically when setting the ease to an AnimationCurve or to a custom ease function
+//     /// </summary>
+//     INTERNAL_Custom,
+//   }
+
 // Easing Types mapping to various functions
 // Taken from https://easings.net/
 UENUM()
