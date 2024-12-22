d84a7e47e2890a3c4ea379e916a9ab90804e415a
Jaiden Gerig <jaideng123@yahoo.com>
2024-12-21 18:44:14 -0800

    More Options Menu Work

---
+++ Source/Bowling/UI/BowlingOptionsMenu.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BowlingOptionsMenu.h"
+
+#include "GameFramework/GameUserSettings.h"
+#include "Kismet/GameplayStatics.h"
+
+void UBowlingOptionsMenu::NativeConstruct()
+{
+	Super::NativeConstruct();
+	FString PlatformName = UGameplayStatics::GetPlatformName();
+	if(PlatformName.Equals("Windows", ESearchCase::IgnoreCase) ||
+		PlatformName.Equals("Mac", ESearchCase::IgnoreCase) ||
+		PlatformName.Equals("Linux", ESearchCase::IgnoreCase))
+	{
+		for(auto Widget : DesktopOnlyWidgets)
+		{
+			Widget->SetVisibility(ESlateVisibility::Visible);
+		}
+		for(auto Widget : MobileOnlyWidgets)
+		{
+			Widget->SetVisibility(ESlateVisibility::Collapsed);
+		}
+		FScreenResolutionArray Resolutions;
+		RHIGetAvailableResolutions(Resolutions, false);
+		for(const auto& Resolution : Resolutions)
+		{
+			
+		}
+		
+		UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+		// Settings->GetScreenResolution();
+		// Settings->SetFullscreenMode(EWindowMode::Fullscreen);
+		// Settings->SetScreenResolution()
+		// Settings->ApplyResolutionSettings(false);
+		// Settings->ApplySettings(false);
+		
+	}
+	else if(PlatformName.Equals("IOS", ESearchCase::IgnoreCase) ||
+		PlatformName.Equals("Android", ESearchCase::IgnoreCase))
+	{
+		for(auto Widget : DesktopOnlyWidgets)
+		{
+			Widget->SetVisibility(ESlateVisibility::Collapsed);
+		}
+		for(auto Widget : MobileOnlyWidgets)
+		{
+			Widget->SetVisibility(ESlateVisibility::Visible);
+		}
+	}
+}
+
+void UBowlingOptionsMenu::RestartGame()
+{
+	// TODO: reset all state instead of reopening the level
+	UGameplayStatics::OpenLevel(GetWorld(), FName(UGameplayStatics::GetCurrentLevelName(GetWorld())));
+}
+
+void UBowlingOptionsMenu::ReturnToTitleScreen()
+{
+	UGameplayStatics::OpenLevel(GetWorld(), TitleMapUrl);
+}
---
+++ Source/Bowling/UI/BowlingOptionsMenu.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
+#include "BowlingOptionsMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOWLING_API UBowlingOptionsMenu : public UUserWidget
+{
+	GENERATED_BODY()
+protected:
+
+	virtual void NativeConstruct() override;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ToggleMotionControlsText;
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<UWidget*> DesktopOnlyWidgets;
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<UWidget*> MobileOnlyWidgets;
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly)
+	FName TitleMapUrl;
+	
+	UFUNCTION(BlueprintCallable)
+	void RestartGame();
+
+	UFUNCTION(BlueprintCallable)
+	void ReturnToTitleScreen();
+};
