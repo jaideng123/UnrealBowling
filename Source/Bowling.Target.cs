0bb2868129f46909d6ebf8826cc10047c62d6ee4
Jaiden Gerig <jaideng123@yahoo.com>
2024-12-24 23:22:15 -0800

    Update Bowling.Target.cs

--- Source/Bowling.Target.cs
+++ Source/Bowling.Target.cs
@@ -11,7 +11,6 @@ public class BowlingTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		ExtraModuleNames.AddRange( new string[] { "Bowling" } );
 		RegisterModulesCreatedByRider();
-		bUseLoggingInShipping = true;
 	}
 
 	private void RegisterModulesCreatedByRider()
